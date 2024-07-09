1c0b096e948d61da26688d05b886aad26e2a0f58
Jorge Almeida Montiel <jorge.pwa.utvt@gmail.com>
2024-07-09 12:28:06 -0600

    Estructura basica del proyecto

---
+++ src/main/java/edu/utvt/examen/ExamenRestApiApplication.java
@@ -0,0 +1,13 @@
+package edu.utvt.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utvt/examen/ExamenRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utvt.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
